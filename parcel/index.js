0699c281e4ca16bd77d449f0b48faf2e21aedf1c
zyb1412 <1412850451@qq.com>
2024-11-28 18:49:38 +0800

    '提交初始代码'

--- parcel/index.js
+++ parcel/index.js
@@ -1,16 +1,4 @@
-import { formatDistanceToNow, format } from "date-fns";
+import { formatDistanceToNow } from "date-fns";
 
 const date = "1996-09-13 10:00:00";
-const targetDate = new Date(date);
-
-// 更新页面上的时间信息
-function updateTime() {
-  const now = new Date();
-  document.body.textContent = `${formatDistanceToNow(targetDate)} ago. Now it's ${format(now, 'yyyy-MM-dd HH:mm:ss')}`;
-}
-
-// 初始时更新时间
-updateTime();
-
-// 每秒更新时间
-setInterval(updateTime, 1000);
+document.body.textContent = `${formatDistanceToNow(new Date(date))} ago`;
